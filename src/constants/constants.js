b4a2c824f929fc55a6f4459979ef87cf8c98c998
Sharan Ainapurapu <sharanainapurapu@gmail.com>
2021-02-09 16:07:47 +0530

    Initial commit

--- src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-export const CONSTANTS = {
-    DESTINATIONLIST: "https://nijinserver.herokuapp.com/dummy/places",
-    WEATHER_API_KEY:"881f596e252c1e56448bc236bddab139",
-    WEATHER_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
-    LIST_TYPE_DESTINATION: "DESTINATIONS",
-    LIST_TYPE_SIMILAR_ITEMS: "SIMILARDESTINATIONS"
-}
