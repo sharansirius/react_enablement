9a0ea0e4cf0bfb9c9a3d19ad2b3353653f0c678d
Sharan Ainapurapu <sharanainapurapu@gmail.com>
2021-02-17 16:51:59 +0530

    Initial Commit

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as BikeRentals } from "./bikerentals/bikerentals";
-export { default as Home } from "./home/home";
-export { default as Hotels } from "./hotels/hotels";
-export { default as Restaurants } from "./restaurants/restaurants";
-export { default as LocationInsight } from "./locationinsight/locationinsight";
-export { default as Page404 } from "./page404/page404";
