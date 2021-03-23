3297c2b6513c16eaf930f1161e6e6f7f5aeaec4c
Sharan Ainapurapu <sharanainapurapu@gmail.com>
2021-03-23 16:04:41 +0530

    Store Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
 import { Home, Login, Movies, NowShowing } from "./screens";
-import "./app.scss";
 import { Header } from "./components";
+import "./App.scss";
 
 function App(): React.ReactElement {
   return (
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Header } from "./header/header";
+/* eslint-disable import/no-cycle */
 export { default as Logo } from "./logo/logo";
 export { default as Menu } from "./menu/menu";
 export { default as User } from "./user/user";
+export { default as Header } from "./header/header";
